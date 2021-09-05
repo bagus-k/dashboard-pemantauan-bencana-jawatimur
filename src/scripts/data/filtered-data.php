4996690a66928368245e2b40483e21ebe8815ea6
bagus-k <82257866+bagus-k@users.noreply.github.com>
2021-09-06 03:06:56 +0700

    Update filtered-data.php

--- src/scripts/data/filtered-data.php
+++ src/scripts/data/filtered-data.php
@@ -1,11 +1,35 @@
 <?php
-$json= json_decode(file_get_contents("data.json"));
-$week = date('Y-m-d h:i:s', strtotime('-10 days'));
+
+<!-- API from BPBD -->
+$BASE_URL =  '';
+$USERNAME = '';
+$PASSWORD = '';
+$APIKEY = '';
+
+$curl = curl_init();
+
+$ch = curl_init($BASE_URL);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml', $additionalHeaders));
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_USERPWD, $USERNAME . ":" . $PASSWORD);
+// curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadName);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$return = curl_exec($ch);
+curl_close($ch);
+
+<!-- Use data.json for local development -->
+<!-- delete code above and change $return below to get_content('./src/scripts/data/data.json') -->
+
+$json= json_decode($return);
+
+$threeDays = date('Y-m-d h:i:s', strtotime('-3 days'));
 $filteredArray = array();
 foreach($json->data as $mydata)
 {
 
-  if($week < $mydata->eventdate) {
+  if($threeDays < $mydata->eventdate || $mydata->status == "BELUM") {
     array_push($filteredArray, $mydata);
   }
 }
